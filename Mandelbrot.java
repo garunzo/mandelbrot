0ea57ecdd73e24caefe83bdb6cd33a7a77066f46
Rob <rob1995@gmail.com>
2017-01-29 10:59:11 -0800

    Continuing to make the main method contain the bare essence of mandelbrot calculations .

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -6,28 +6,26 @@ public class Mandelbrot {
     public static void main(String[] args) throws Exception {
         int iterations;
 
-        PlottingParameters params = new PlottingParameters(args);
-        if (!params.valid) return;
+        PlottingParameters pp = new PlottingParameters(args);
+        if (!pp.valid) return;
 
-        int width = 1920*params.zoom, height = 1080*params.zoom, max = 1000;
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int  max = 1000;
+        BufferedImage image = new BufferedImage(pp.width, pp.height, BufferedImage.TYPE_INT_RGB);
 
         ColorMap cm = new ColorMap(max);
 
-        for (int row = 0; row < height; row++) {  // Loop over every position on screen / image
-            for (int col = 0; col < width; col++) {
-                ComplexNumber C = params.translateScreenPosToComplexNumberPlane(col, row, width, height);
+        do {
+                ComplexNumber C = pp.currentScreenPosToComplexNumber();
                 ComplexNumber Z = new ComplexNumber(0.0, 0.0); // 0 + 0i
 
                 /* Iterate while magnitude of Z < 2 (or Z^2 < 4; avoids slow square root operation) */
                 for (iterations = 0; Z.magnitudeSquared() < 4 && iterations < max; iterations++) {
                     Z.squareComplex().addComplex(C); // f(Z) = Z^2 + C
                 }
-                // If iteration < max, then not in set, so set a color, otherwise black
-                if (iterations < max) image.setRGB(col, row, cm.getColor(iterations));
-                else image.setRGB(col, row, cm.black());
-            }
-        }
-        ImageIO.write(image, "png", new File(params.pathName(args)));
+                image.setRGB(pp.col, pp.row, (iterations < max) ? cm.getColor(iterations) : cm.black());
+
+        } while (pp.nextScreenPos());
+
+        ImageIO.write(image, "png", new File(pp.pathName(args)));
     }
 }
--- PlottingParameters.java
+++ PlottingParameters.java
@@ -5,6 +5,10 @@ public class PlottingParameters {
    public double offsetX; // x translation
    public double offsetY; // y translation
    public boolean valid;
+   public final int width;
+   public final int height;
+   public int col = 0;
+   public int row = 0;
 
 
    public PlottingParameters(String[] args) {
@@ -13,10 +17,15 @@ public class PlottingParameters {
          offsetX = Double.parseDouble(args[1]);
          offsetY = Double.parseDouble(args[2]);
          zoom = Integer.parseInt(args[3]);
+         width = 1920*zoom;
+         height = 1080*zoom;
+
       } else {
          System.out.println("Usage: Mandelbrot <scale> <x-offset> <y-offset> <pixels>");
          System.out.println("Example: Mandelbrot 4.0 0.0 0.0 1");
          valid = false;
+         width = 0; // being lazy
+         height = 0;
       }
       valid = true;
    }
@@ -27,9 +36,24 @@ public class PlottingParameters {
       return new ComplexNumber(c_re, c_im);
    }
 
+    public ComplexNumber currentScreenPosToComplexNumber() {
+        double c_re = (col - width/2)*scale/width - offsetX;
+        double c_im = (row - height/2)*scale/width + offsetY;
+        return new ComplexNumber(c_re, c_im);
+    }
+
    public String pathName(String[] args) {
       return "images/mandelbrot-S-"
               + args[0] + "-X-" + args[1]
               + "-Y-" + args[2] + "-P-" + this.zoom + ".png";
+   }
+
+   public boolean nextScreenPos() {
+       col++;
+       if (col == width) {
+           row++;
+           col = 0;
+       }
+       return (row < height);
    }
 }
