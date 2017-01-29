2f9aea21b4fd80adf86a7654fa5755b240638aca
Rob <rob1995@gmail.com>
2017-01-29 14:58:47 -0800

    Eliminate even more of the details around imaging, writing to files, etc.

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,31 +1,20 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-
 public class Mandelbrot {
     public static void main(String[] args) throws Exception {
         int iterations;
-
-        PlottingParameters pp = new PlottingParameters(args);
-        if (!pp.valid) return;
-
-        int  max = 1000;
-        BufferedImage image = new BufferedImage(pp.width, pp.height, BufferedImage.TYPE_INT_RGB);
-
-        ColorMap cm = new ColorMap(max);
+        PlottingParameters pp = new PlottingParameters(args, 1000);
 
         do {
-                ComplexNumber C = pp.currentScreenPosToComplexNumber();
-                ComplexNumber Z = new ComplexNumber(0.0, 0.0); // 0 + 0i
+            ComplexNumber C = pp.currentScreenPosToComplexNumber();
+            ComplexNumber Z = new ComplexNumber(0.0, 0.0); // 0 + 0i
 
-                /* Iterate while magnitude of Z < 2 (or Z^2 < 4; avoids slow square root operation) */
-                for (iterations = 0; Z.magnitudeSquared() < 4 && iterations < max; iterations++) {
-                    Z.squareComplex().addComplex(C); // f(Z) = Z^2 + C
-                }
-                image.setRGB(pp.col, pp.row, (iterations < max) ? cm.getColor(iterations) : cm.black());
+            /* Iterate while magnitude of Z < 2 (or Z^2 < 4; to avoid slow square root operation) */
+            for (iterations = 0; Z.magnitudeSquared() < 4 && iterations < pp.max; iterations++) {
 
+                Z.squareComplex().addComplex(C); // Z = Z^2 + C
+            }
+            pp.setColorCurrentScreenPos(iterations);
         } while (pp.nextScreenPos());
 
-        ImageIO.write(image, "png", new File(pp.pathName(args)));
+        pp.writeImageToFile();
     }
 }
--- PlottingParameters.java
+++ PlottingParameters.java
@@ -1,3 +1,7 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class PlottingParameters {
 
    public int zoom;       // amount of additional zooming
@@ -5,27 +9,43 @@ public class PlottingParameters {
    public double offsetX; // x translation
    public double offsetY; // y translation
    public boolean valid;
-   public final int width;
-   public final int height;
+   public int width;
+   public int height;
    public int col = 0;
    public int row = 0;
+   public int max = 1000;
+   public ColorMap cm;
+   public BufferedImage image;
+   public String[] orig_args;
 
 
-   public PlottingParameters(String[] args) {
+
+   public PlottingParameters(String[] args, int m) {
       if (args.length == 4) {
-         scale = Double.parseDouble(args[0]);
-         offsetX = Double.parseDouble(args[1]);
-         offsetY = Double.parseDouble(args[2]);
-         zoom = Integer.parseInt(args[3]);
-         width = 1920*zoom;
-         height = 1080*zoom;
+          try {
+              scale = Double.parseDouble(args[0]);
+              offsetX = Double.parseDouble(args[1]);
+              offsetY = Double.parseDouble(args[2]);
+              zoom = Integer.parseInt(args[3]);
+              width = 1920 * zoom;
+              height = 1080 * zoom;
+              cm = new ColorMap(max);
+              image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+              max = m;
+              orig_args = args;
+          } catch (NumberFormatException e) {
+              System.out.println("Scale and offsets must be floating point.");
+              System.out.println("Zoom must be integer.");
+              System.exit(1);
+          }
 
       } else {
-         System.out.println("Usage: Mandelbrot <scale> <x-offset> <y-offset> <pixels>");
+         System.out.println("Usage: Mandelbrot <scale> <x-offset> <y-offset> <zoom>");
          System.out.println("Example: Mandelbrot 4.0 0.0 0.0 1");
          valid = false;
          width = 0; // being lazy
          height = 0;
+         System.exit(1);
       }
       valid = true;
    }
@@ -56,4 +76,13 @@ public class PlottingParameters {
        }
        return (row < height);
    }
+
+   public void setColorCurrentScreenPos(int iterations) {
+       image.setRGB(col, row, (iterations < max) ? cm.getColor(iterations) : cm.black());
+   }
+
+   public void writeImageToFile() throws Exception {
+       ImageIO.write(image, "png", new File(pathName(orig_args)));
+   }
+
 }
